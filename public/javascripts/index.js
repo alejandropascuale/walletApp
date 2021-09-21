c2238afa24ee92f8c3c8861f4697e5880963c0e1
alejandropascuale <apascuale@outlook.com>
2021-09-21 15:12:23 -0300

    install swiper and add home section widt slider

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -21,3 +21,16 @@ closeIcon.onclick = () => {
   searchForm.classList.remove('active');
 };
 
+const swiper = new Swiper(".home-slider", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  loop: true,
+});
