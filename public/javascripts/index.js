25adfd2552cb261598d23ed3a6f48ac9c9479426
alejandropascuale <apascuale@outlook.com>
2021-09-22 19:01:28 -0300

    create footer and include scripts in this file

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -34,13 +34,3 @@ const swiper = new Swiper(".home-slider", {
   },
   loop: true,
 });
-
-let avatarUser = document.querySelector('.icon-user');
-let userMenu = document.querySelector('.user-menu');
-
-avatarUser.addEventListener ('mouseover', () => {
-  userMenu.style.display = 'block';
-})
-userMenu.addEventListener ('mouseleave', () => {
-  userMenu.style.display = 'none';
-})
