aed7baf46fd1349ca03d6aa2a66bcedc087f2cc6
alejandropascuale <apascuale@outlook.com>
2021-09-23 12:20:30 -0300

    change in the way scripts are passed to view

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -20,17 +20,3 @@ searchIcon.onclick = () => {
 closeIcon.onclick = () => {
   searchForm.classList.remove('active');
 };
-
-const swiper = new Swiper(".home-slider", {
-  spaceBetween: 30,
-  centeredSlides: true,
-  autoplay: {
-    delay: 7500,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  loop: true,
-});
--- public/javascripts/operationsMenu.js
+++ public/javascripts/operationsMenu.js
@@ -1,14 +1,18 @@
 window.addEventListener('load', ()=> {
     let addButton = document.querySelector('#add-operations-button');
-    let operationsAddForm = document.querySelector('#add-operations-form')
-    let cancelButton = document.querySelector('#cancel-button')
+    let operationsAddForm = document.querySelector('#add-operations-form');
+    let cancelButton = document.querySelector('#cancel-button');
+    let detailInput = document.querySelector('#detail');
 
 
     addButton.addEventListener('click', ()=> {
         operationsAddForm.style.left = '0';
+        detailInput.focus();
     })
     cancelButton.addEventListener('click', ()=>{
         operationsAddForm.style.left = '-110%';
     })
+
+    /* let optionCategory =  */
 
 })
