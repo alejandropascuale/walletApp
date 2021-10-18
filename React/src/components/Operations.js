90529bda5c636ab6d100a39b25b5982746fd21e1
alejandropascuale <apascuale@outlook.com>
2021-10-18 17:11:43 -0300

    add components to operations components

--- React/src/components/Operations.js
+++ React/src/components/Operations.js
@@ -1,13 +1,20 @@
 import React from 'react'
+import Header from './Header';
+import FormSearch from './FormSearch';
 import BalanceSection from './BalanceSection'
+import LastOperations from './LastOperations'
+import Footer from './Footer';
 
 
 
 function Operations() {
     return (
         <main>
+            <Header />
+            <FormSearch />
             <BalanceSection />
-      
+            <LastOperations />
+            <Footer />
         </main>
     )
 }
