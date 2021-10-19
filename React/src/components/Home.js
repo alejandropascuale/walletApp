5084a40c8f01b2f55e85d93ae94aeb5f4a9b30a2
alejandropascuale <apascuale@outlook.com>
2021-10-19 13:52:47 -0300

    add conditional render in lastOperations to chose the information inside of table

--- React/src/components/Home.js
+++ React/src/components/Home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 import Header from './header/Header';
 import FormSearch from './FormSearch';
--- React/src/components/UserLastOperations.js
+++ React/src/components/UserLastOperations.js
@@ -25,28 +25,22 @@ function LastOperations() {
             return "";
           }
           let emailCookie = getCookie('userEmail');
-          let lastOperations = [];
-          if(emailCookie){
-            const user = data.find(u => u.email === emailCookie);
-            fetch(`http://localhost:3001/api/operations/user/${user.idUser}`)
-                .then(response => response.json())
-                .then(op => {
-                  if(op.length > 10){
-                    lastOperations = op.slice(op.length-10).reverse();
-                    } else {
-                      op.slice(op.length).reverse()
-                      lastOperations = op.slice(op.length).reverse();
-                    }
-                    setOperations(lastOperations)
-                })
-          } else {
-            setOperations(lastOperations);
-          }
-          
+          const user = data.find(u => u.email === emailCookie);
         })
+        fetch(`http://localhost:3001/api/operations/user/${user.idUser}`)
+            .then(response => response.json())
+            .then(data => {
+              let lastOperations = [];
+              if(data.length > 10){
+                lastOperations = data.slice(data.length-10).reverse();
+                } else {
+                  data.slice(data.length).reverse()
+                  lastOperations = data.slice(data.length).reverse();
+                }
+                setOperations(lastOperations)
+            })
     }, [])
-    if(operations.length !== 0){
-      return (
+    return (
         <section className="last-operations" id="last-operations">
         <h1 className="heading">Last Operations</h1>
         
@@ -68,6 +62,11 @@ function LastOperations() {
               </tr>
             </tfoot>
             <tbody id="insert-data">
+              {/* <tr>
+                <td>
+                  you have no registered operations
+                </td>
+              </tr> */}
               {operations.map((operation, i) => {
                 return (
                   <tr key={i}>
@@ -81,41 +80,7 @@ function LastOperations() {
           </table>
         </div>
       </section>
-      )
-    } else {
-      return (
-        <section className="last-operations" id="last-operations">
-        <h1 className="heading">Last Operations</h1>
-        
-        <Link to='/operations'><h3 className="sub-heading heartbeat">Click here to manage your operations</h3></Link>
-        <div className="card-table">
-          <table className="home-table"aria-describedby="myOperations" id="dataTable" cellspacing="0">
-            <thead>
-              <tr>
-                <th id="detail">Detail</th>
-                <th id="amount">Amount</th>
-                <th id="date">Date</th>
-              </tr>
-            </thead>
-            <tfoot>
-              <tr>
-                <th id="detail">Detail</th>
-                <th id="amount">Amount</th>
-                <th id="date">Date</th>
-              </tr>
-            </tfoot>
-            <tbody id="insert-data">
-              <tr>
-                <td>
-                  you have no registered operations
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </section>
-      )
-    }
+    )
 }
 
 export default LastOperations;
