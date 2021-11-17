8a6c68c58f5624d500a04dbe6de73de9ba2193f9
alejandropascuale <apascuale@outlook.com>
2021-11-17 17:10:33 -0300

    add configuration to production

--- database/config/config.js
+++ database/config/config.js
@@ -14,10 +14,10 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": "b40abee22f357f",
+    "password": "dcf36a7c",
+    "database": "heroku_689aa9aebaf90c3",
+    "host": "us-cdbr-east-04.cleardb.com/",
     "dialect": "mysql"
   }
 }
